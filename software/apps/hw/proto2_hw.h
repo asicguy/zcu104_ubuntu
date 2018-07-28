ab9b548a4701a6c043a29341cd59602609ceb9c9
hdlguy <hdlguy@hotmail.com>
2018-07-28 14:39:17 -0600

    bram_test app writes LED and tests bram.

--- software/apps/hw/proto2_hw.h
+++ software/apps/hw/proto2_hw.h
@@ -7,7 +7,7 @@
 #define 	FPGA_VERSION		AXI_REGFILE_0+0x04*1  // Returns major and minor version numbers.
 #define 	LED_CONTROL		AXI_REGFILE_0+0x04*2  // Returns major and minor version numbers.
 
-#define     TEST_BRAM_OFFSET    0x20000000
-#define     TEST_BRAM_SIZE      0x00001000 // 4KB
+#define         TEST_BRAM_OFFSET    0x20000000
+#define         TEST_BRAM_SIZE      0x00001000 // 4KB
 
 
