f3bc61a6d06013d26e805d12fd47268964063ed0
hdlguy <hdlguy@hotmail.com>
2018-08-16 18:49:01 -0600

    added matrix inverter addresses to header file.

--- software/apps/hw/proto2_hw.h
+++ software/apps/hw/proto2_hw.h
@@ -10,4 +10,17 @@
 #define         TEST_BRAM_OFFSET    0x20000000
 #define         TEST_BRAM_SIZE      0x00001000 // 4KB
 
+#define         AXI_MATINV          0x00010000 // This is the AXI control bus into the matrix inverter.
+
+#define         MATINV_CONTROL      AXI_MATINV+0x04*0
+// bit 0 = ap_start  (read/write/COH)
+// bit 1 = ap_done   (read/COR)
+// bit 2 = ap_idle   (read)
+// bit 3 = ap_ready  (read)
+// bit 7 = auto_restart (read/write)
+
+#define         MATINV_IBRAM        0x00020000 // This is the input bram of the matrix inverter.
+#define         MATINV_OBRAM        0x00030000 // This is the output bram of the matrix inverter.
+
+
 
